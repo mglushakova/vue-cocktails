d417efb324586767435f0fd696d4c9fa61498a0a
Marina Glushakova <marina.glushakova@byteminds.co.uk>
2023-06-30 14:10:37 +0500

    configures router

---
+++ src/pages/Cocktail.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<template>Cocktail</template>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main.sass'
+</style>
---
+++ src/pages/CocktailRandom.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<template>CocktailRandom</template>
+
+<style lang="sass" scoped>
+@import '../assets/styles/main.sass'
+</style>
